5d5b25b83b0873b85b1baa15d3175169e84445d7
Melody Wei <melodywei861016@gmail.com>
2017-10-02 20:41:14 -0700

    second commit

--- rails-part-one/app/controllers/members_controller.rb
+++ rails-part-one/app/controllers/members_controller.rb
@@ -1,12 +1,19 @@
 class MembersController < ApplicationController
+ 	def index
+    	@members = Member.where mirror_id: params[:mirror_id]
+    end
+
 	def create
     	@mirror = Mirror.find(params[:mirror_id])
     	@member = @mirror.members.create(member_params)
     	redirect_to mirror_path(@mirror)
   	end
- 
+
   	private
    		def member_params
      		params.require(:member).permit(:first_name, :last_name, :activity)
     	end
+
+    def activity
+    	Member.find(params[:id)]).activity
 end
--- rails-part-one/config/routes.rb
+++ rails-part-one/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :mirrors
+  resources :mirrors do
+  	resources :members
 end
